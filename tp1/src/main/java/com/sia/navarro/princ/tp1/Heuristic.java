02f5584ba7453dd7bab2b6fc6adecde678953351
Florencia Monti <mamonti@itba.edu.ar>
2020-08-19 18:46:19 -0300

    Added new heuristic

--- tp1/src/main/java/com/sia/navarro/princ/tp1/Heuristic.java
+++ tp1/src/main/java/com/sia/navarro/princ/tp1/Heuristic.java
@@ -1,8 +1,11 @@
 package com.sia.navarro.princ.tp1;
 
+import java.util.*;
+
 public class  Heuristic {
     private final static String PB = "pb";
     private final static String BG = "bg";
+    private final static String BGW = "bgw";
     private String type;
 
     public Heuristic(String type){
@@ -14,6 +17,8 @@ public class  Heuristic {
             return this.playerToBoxes(node);
         } else if (BG.equals(type)){
             return this.boxesToGoals(node);
+        } else if (BGW.equals(type)){
+            return this.boxesToGoalsWalls(node);
         }
         return this.combination(node);
     }
@@ -66,5 +71,77 @@ public class  Heuristic {
             h = h + haux;
         }
         return h / boxes.length;
+    }
+
+    private int boxesToGoalsWalls(Node node){
+        Board board = node.getBoard();
+        if(board.isStuck())
+            return 1000000000;
+        Box[] boxes = board.getBoxes();
+        Position[] goals = board.getWinPoints();
+        int haux = 1000000000;
+        int i;
+        int h = 0;
+        for (Box b: boxes) {
+            for (Position g : goals) {
+                i = movesToGoal(node, b.getPos(), g);
+                if (i < haux){
+                    haux = i;
+                }
+            }
+            h = h + haux;
+        }
+        return h / boxes.length;
+    }
+
+    private int movesToGoal(Node node, Position box, Position goal){
+        Board board = node.getBoard();
+        if(board.isStuck())
+            return 1000000000;
+        Box[] b = new Box[1];
+        b[0] = new Box(box);
+        Position[] g = new Position[1];
+        g[0] = goal;
+        Board bb = new Board(board.getPlayer(), b, g, board.getWalls(), board.getSize(), board.getCost());
+        return this.bfs(bb);
+    }
+
+    private int bfs(Board board) {
+        LinkedList<Board> firstBoard = new LinkedList<Board>();
+        firstBoard.add(board.cloneBoard());
+        Node init = new Node(firstBoard);
+        HashSet<Board> repeated = new HashSet<Board>();
+        repeated.add(board.cloneBoard());
+        boolean hasWon = false;
+        Queue<Node> bfsQueue = new PriorityQueue<Node>(11, new Comparator<Node>() {
+            public int compare(Node o1, Node o2) {
+                return o1.getDepth() - o2.getDepth();
+            }
+        });
+        bfsQueue.add(init);
+        Node aux;
+
+        while (bfsQueue.size() != 0 && !hasWon) {
+            aux = bfsQueue.poll();
+            hasWon = aux.hasWon();
+            if (hasWon) {
+                return repeated.size();
+            } else {
+                LinkedList<Node> auxList = aux.getNextNodes();
+                if (auxList.size() == 0) {
+                } else {
+                    for(Node n: auxList) {
+                        if(!repeated.contains(n.getBoard())) {
+                            repeated.add(n.getBoard());
+                            bfsQueue.add(n);
+                        }
+                    }
+                }
+            }
+        }
+        if (!hasWon) {
+            return 1000000000;
+        }
+        return repeated.size();
     }
 }
