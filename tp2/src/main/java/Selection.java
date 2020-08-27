79eed4f62be354ba06d371a616ebc4fef6dc9e87
rodrigo <rnavarro@itba.edu.ar>
2020-08-27 20:27:53 -0300

    Universal

--- tp2/src/main/java/Selection.java
+++ tp2/src/main/java/Selection.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 public class Selection {
 
     public Player[] roulette(Player[] players, int k){
@@ -21,6 +23,37 @@ public class Selection {
             double random = Math.random();
             for (int j = 1; j < accumulated.length; j++) {
                 if (accumulated[j] > random) {
+                    aux[i] = players[j-1];
+                }
+            }
+        }
+
+        return aux;
+    }
+
+    public Player[] universal(Player[] players, int k){
+        Player[] aux = new Player[k];
+        double total = 0;
+        double[] relative = new double[aux.length];
+        double[] accumulated = new double[aux.length];
+
+        for (Player p : players) {
+            total+= p.performance();
+        }
+
+        for (int i = 0; i < relative.length; i++) {
+            relative[i] = players[i].performance()/total;
+        }
+        accumulated[0] = relative[0];
+        for (int i = 1; i < accumulated.length; i++) {
+            accumulated[i] = relative[i] + accumulated[i-1];
+        }
+        double random = Math.random();
+
+        for (int i = 0; i < k; i++) {
+            double r = (random + i)/k;
+            for (int j = 1; j < accumulated.length; j++) {
+                if (accumulated[j] > r) {
                     aux[i] = players[j-1];
                 }
             }
