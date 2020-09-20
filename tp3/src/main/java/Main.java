33e38658f43d830fc5aabaaeed22b09b6c5e13cf
Guido Princ <guido.princ@wolox.com.ar>
2020-09-20 02:06:35 -0300

    txt reading

---
+++ tp3/src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("TP3-ej2-Conjunto-entrenamiento.txt");
+        ArrayList<Double[]> aux = new ArrayList<>();
+        aux = TxtReader.getDoubleArrayFromTxt(file, 3);
+
+        for (Double[] i: aux) {
+            for (Double j: i){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        return;
+    }
+}
