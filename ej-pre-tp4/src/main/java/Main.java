fe328f9fc8995c9e81efb4d580f16f26896d0b3f
Guido Princ <guido.princ@wolox.com.ar>
2020-10-08 17:40:25 -0300

    added csv reading

--- ej-pre-tp4/src/main/java/Main.java
+++ ej-pre-tp4/src/main/java/Main.java
@@ -1,7 +1,24 @@
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        File csvFile = new File("europe.csv");
+        BufferedReader csvReader;
+        if (csvFile.isFile()) {
+            try {
+                csvReader = new BufferedReader(new FileReader("europe.csv"));
+                String row;
+                while ((row = csvReader.readLine()) != null) {
+                    String[] data = row.split(",");
+                }
+                csvReader.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         return;
 
