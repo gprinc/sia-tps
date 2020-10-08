c6677bf889b0f4db33f00e6423644b29c9922cae
Guido Princ <guido.princ@wolox.com.ar>
2020-10-08 20:45:56 -0300

    added item

---
+++ ej-pre-tp4/src/main/java/Item.java
@@ -0,0 +1,68 @@
+import java.util.Objects;
+
+public class Item {
+    private String country;
+    private String area;
+    private String gdp;
+    private String inflation;
+    private String lifeExpect;
+    private String military;
+    private String popGrowth;
+    private String unemployment;
+
+    public Item(String country, String area, String gdp, String inflation, String lifeExpect, String military, String popGrwoth, String unemployment) {
+        this.country = country;
+        this.area = area;
+        this.gdp = gdp;
+        this.inflation = inflation;
+        this.lifeExpect = lifeExpect;
+        this.military = military;
+        this.popGrowth = popGrwoth;
+        this.unemployment = unemployment;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public String getGdp() {
+        return gdp;
+    }
+
+    public String getInflation() {
+        return inflation;
+    }
+
+    public String getLifeExpect() {
+        return lifeExpect;
+    }
+
+    public String getMilitary() {
+        return military;
+    }
+
+    public String getPopGrowth() {
+        return popGrowth;
+    }
+
+    public String getUnemployment() {
+        return unemployment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return this.country.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(country);
+    }
+}
--- ej-pre-tp4/src/main/java/Main.java
+++ ej-pre-tp4/src/main/java/Main.java
@@ -1,20 +1,33 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
         File csvFile = new File("europe.csv");
         BufferedReader csvReader;
+        String[] data;
+        ArrayList<Item> countries = new ArrayList<>();
         if (csvFile.isFile()) {
             try {
                 csvReader = new BufferedReader(new FileReader("europe.csv"));
                 String row;
                 boolean isFirstLine = true;
                 while ((row = csvReader.readLine()) != null) {
+                    data = row.split(",");
                     if (isFirstLine) {
                         isFirstLine = false;
                     } else {
-                        String[] data = row.split(",");
+                        data = row.split(",");
+                        String country = data[0];
+                        String area = data[1];
+                        String gdp = data[2];
+                        String inflation = data[3];
+                        String lifeExpect = data[4];
+                        String military = data[5];
+                        String popGrowth = data[6];
+                        String unemployment = data[7];
+                        countries.add(new Item(country, area, gdp, inflation, lifeExpect, military, popGrowth, unemployment));
                     }
                 }
                 csvReader.close();
@@ -23,6 +36,15 @@ public class Main {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            Item i = countries.get(0);
+            System.out.println(i.getCountry());
+            System.out.println(i.getArea());
+            System.out.println(i.getGdp());
+            System.out.println(i.getInflation());
+            System.out.println(i.getLifeExpect());
+            System.out.println(i.getMilitary());
+            System.out.println(i.getPopGrowth());
+            System.out.println(i.getUnemployment());
         }
 
         return;
