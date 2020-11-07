89f58d80d6ccf612f8351a907d71bbad93c65adb
Guido Princ <guido.princ@wolox.com.ar>
2020-11-07 20:12:25 -0300

    added chart

---
+++ tp5/src/main/java/JFreeDraw.java
@@ -0,0 +1,100 @@
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.block.BlockBorder;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.chart.title.TextTitle;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class JFreeDraw extends JFrame {
+
+    public JFreeDraw() {
+        initUI();
+    }
+
+    private void initUI() {
+        int[][] data = new int[0][0];
+        XYDataset dataset = createDataset(data);
+        JFreeChart chart = createChart(dataset);
+
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        chartPanel.setBackground(Color.white);
+        add(chartPanel);
+
+        pack();
+        setTitle("Line chart");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private XYDataset createDataset(int[][] data) {
+
+        XYSeries series = new XYSeries("Layer");
+        // TODO use data
+        series.add(18, 567);
+        series.add(18, 612);
+        series.add(25, 612);
+        series.add(25, 567);
+        series.add(18, 567);
+
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(series);
+
+        return dataset;
+    }
+
+    private JFreeChart createChart(XYDataset dataset) {
+        
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                "Average salary per age",
+                "X",
+                "Y",
+                dataset,
+                PlotOrientation.VERTICAL,
+                true,
+                true,
+                false
+        );
+
+        XYPlot plot = chart.getXYPlot();
+
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        renderer.setSeriesPaint(0, Color.RED);
+        renderer.setSeriesStroke(0, new BasicStroke(2.0f));
+
+        plot.setRenderer(renderer);
+        plot.setBackgroundPaint(Color.white);
+
+        plot.setRangeGridlinesVisible(true);
+        plot.setRangeGridlinePaint(Color.BLACK);
+
+        plot.setDomainGridlinesVisible(true);
+        plot.setDomainGridlinePaint(Color.BLACK);
+
+        chart.getLegend().setFrame(BlockBorder.NONE);
+
+        chart.setTitle(new TextTitle("TODO CHANGE THIS",
+                        new Font("Serif", java.awt.Font.BOLD, 18)
+                )
+        );
+
+        return chart;
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+
+            JFreeDraw ex = new JFreeDraw();
+            ex.setVisible(true);
+        });
+    }
+}
